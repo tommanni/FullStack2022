e2f39ea84327b6138df910f3dd075c7348930cbe
tommanni <108432153+tommanni@users.noreply.github.com>
2022-10-04 16:59:28 +0300

    ok

--- testi.js
+++ testi.js
@@ -3,7 +3,7 @@ Tee ohjelma, joka tallentaa yhden viikon viikonpäivät ja niitä vastaavat työ
 taulukkoon. Voit keksiä päiville haluamasi työtunnit, rehellinen pitää kuitenkin olla. */
 
 const paivienTyöTunnit = () => {
-    paivat = [{ "paiva": "Maanantai", "tunnit": 8 }, { "paiva": "Tiistai", "tunnit": 8 }, { "paiva": "Keskiviikko", "tunnit": 8 }, { "paiva": "Torstai", "tunnit": 8 }, { "paiva": "Perjantai", "tunnit": 8 }, { "paiva": " Lauantai", "tunnit": 4 }, { "paiva": "Sunnuntai", "tunnit": 4 }]
+    paivat = [{ "paiva": "Maanantai", "tunnit": 11 }, { "paiva": "Tiistai", "tunnit": 7 }, { "paiva": "Keskiviikko", "tunnit": 8 }, { "paiva": "Torstai", "tunnit": 7 }, { "paiva": "Perjantai", "tunnit": 9 }, { "paiva": " Lauantai", "tunnit": 2 }, { "paiva": "Sunnuntai", "tunnit": 3 }]
 
 
     /* Tehtävä 2.2
@@ -14,14 +14,31 @@ const paivienTyöTunnit = () => {
     //a)
     tuntiSumma = 0
     paivat.forEach((paiva) => { tuntiSumma += paiva.tunnit })
-    console.log(tuntiSumma / paivat.length)
+    let tuntiKeskiarvo = tuntiSumma / paivat.length
+    console.log("Tuntien keskiarvo forEachilla:", tuntiKeskiarvo)
 
     //b)
     const tuntiKeskiarvo2 = paivat.reduce((previousValue, currentValue) => previousValue + currentValue.tunnit, 0) / paivat.length
-    console.log(tuntiKeskiarvo2)
+    console.log("Tuntien keskiarvo reducella:", tuntiKeskiarvo2)
 
+    /* Tehtävä 2.3
+    Lisää ohjelmaan 2.2 minimin ja maksimin selvittävä osuus.
+    a) for-rakenteen (forEach, for-silmukka tai for of) avulla
+    b) reduce -funktiolla */
 
+    //a)
+    let pienin = paivat[0].tunnit
+    let suurin = paivat[0].tunnit
+    paivat.forEach((paiva) => paiva.tunnit < pienin ? pienin = paiva.tunnit : pienin = pienin)
+    paivat.forEach((paiva) => paiva.tunnit > suurin ? suurin = paiva.tunnit : suurin = suurin)
+    console.log("Pienin tuntimäärä forEachilla:", pienin)
+    console.log("Suurin tuntimäärä forEachilla:", suurin)
 
+    //b)
+    let pienin2 = paivat.reduce((acc, currentValue) => currentValue.tunnit < acc ? currentValue.tunnit : acc, paivat[0].tunnit)
+    let suurin2 = paivat.reduce((acc, currentValue) => currentValue.tunnit > acc ? currentValue.tunnit : acc, paivat[0].tunnit)
+    console.log("Pienin tuntimäärä reducella:", pienin2)
+    console.log("Suurin tuntimäärä reducella:", suurin2)
 }
 
-paivienTyöTunnit()
+paivienTyöTunnit();
